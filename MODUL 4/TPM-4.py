c0dc316da9e34e43ea1ef404a8b36f75870394b7
AzzayanaRisnanda11 <azzayanarisnanda0@gmail.com>
2024-12-17 19:39:45 +0800

    Add files via upload

---
+++ MODUL 4/TPM-4.py
@@ -0,0 +1,9 @@
+# input tinggi badan (dalam meter) dan berat badan (dalam kilogram)
+tinggi = 1.6 
+berat = 57.6
+
+# menghitung BMI
+BMI = berat / (tinggi **2)
+
+# hasil BMI dengan format string
+print(f"skor BMI adalah:{BMI:.1f}")
